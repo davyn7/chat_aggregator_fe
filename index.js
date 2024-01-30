facb3ec5f4561e5799a31a1ecffa1b052b82d4de
Davyn Sudirdjo <davyn@Davyns-MacBook.local>
2024-01-30 22:10:18 +0700

    Bugfixing

--- index.js
+++ index.js
@@ -36,8 +36,8 @@ app.post("/webhook", (req, res) => {
     if (body_param.object) {
         if (body_param.entry && 
             body_param.entry[0].changes &&
-            body_param.entry[0].changes[0].value.message &&
-            body_param.entry[0].changes[0].value.message[0]) {
+            body_param.entry[0].changes[0].value.messages &&
+            body_param.entry[0].changes[0].value.messages[0]) {
 
             let phone_number_id = body_param.entry[0].changes[0].value.metadata.phone_number_id;
             let sender = body_param.entry[0].changes[0].value.messages[0].from;
